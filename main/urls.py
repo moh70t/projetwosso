e77b505c3ad4fd3e33f93a4c4af1af18526f561a
Mohamed-ux-lab <koureissym@gmail.com>
2024-06-26 22:31:42 +0000

    Design du site

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path 
 from .views import index, product, about, contact, add_to_cart, view_cart, checkout, register
 
 urlpatterns = [
@@ -9,6 +9,6 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', view_cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
-    path('accounts/', include('django.contrib.auth.urls')),  # Ajoutons cette ligne pour inclure les vues d'authentification intégrées de Django
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', register, name='register'),
 ]
